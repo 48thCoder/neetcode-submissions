163a263c7bacd40619b57f599d65a6ed515aa1fd
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-09-21 00:06:50 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n + 1, 0);
+
+        for (int x : nums) {
+            hash[x] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+        return n;
+    }
+};
